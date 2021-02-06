35a5a08718d585a47c0cb1d6edbe226fb59f5627
Vinicius <vinnicius.jimenez@gmail.com>
2021-02-06 01:44:02 -0300

    Finished

--- form_email/scripts.js
+++ form_email/scripts.js
@@ -1,17 +1,11 @@
 (async() => {
     // (Object) -> { sigla: string, nome: string }
-    const optiontag = ({ sigla, nome }) => `<option value=${sigla}>${nome}</option>`
+    const optiontag = ({ sigla, nome }, selected) => `<option value=${sigla}>${nome}</option>`
 
     // (Array, String)
-    const filterRegion = (states, sigla) => 
-        states.filter(state => state.regiao.sigla === sigla).sort((state1, state2) => {
-            if(state1.nome > state2.nome)
-                return 1
-            if(state1.nome < state2.nome)
-                return -1
-            
-            return 0
-        })        
+    const filterRegion = (states, sigla) =>  
+        states.filter(state => state.regiao.sigla === sigla)
+              .sort(({ nome: nome1 }, { nome: nome2 }) => nome1.localeCompare(nome2))
 
     // (String, Array) 
     const createOptGroup = (region, options) => {
@@ -33,17 +27,15 @@
 
     const states = await (await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')).json()
 
-    const regions = {
-        centerwest: { label: 'Região Centro-Oeste', states: filterRegion(states, 'CO') },
-        northeast: { label: 'Região Nordeste', states: filterRegion(states, 'NE') },
-        north: { label: 'Região Norte', states: filterRegion(states, 'N') },
-        southeast: { label: 'Região Sudeste', states: filterRegion(states, 'SE') },
-        south: { label: 'Região Sul', states: filterRegion(states, 'S') },
-    }
-
-    Object.keys(regions).forEach(key => {
-        const region = regions[key]
-        
+    const regions = [
+        { label: 'Região Centro-Oeste', states: filterRegion(states, 'CO') },
+        { label: 'Região Nordeste', states: filterRegion(states, 'NE') },
+        { label: 'Região Norte', states: filterRegion(states, 'N') },
+        { label: 'Região Sudeste', states: filterRegion(states, 'SE') },
+        { label: 'Região Sul', states: filterRegion(states, 'S') },
+    ]
+  
+    regions.forEach(region => {
         const optgroup = createOptGroup(region.label, region.states.map(state => optiontag(state)))
 
         addOptGroup(optgroup)
